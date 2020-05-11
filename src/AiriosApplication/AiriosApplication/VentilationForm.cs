3c709b6365ea9ed669ecd5c4c51735cf034bd56d
I415849 <i415849@fhict.nl>
2020-05-11 13:44:56 +0200

    Added comments

--- src/AiriosApplication/AiriosApplication/VentilationForm.cs
+++ src/AiriosApplication/AiriosApplication/VentilationForm.cs
@@ -24,27 +24,30 @@ namespace AiriosApplication
       public VentilationForm()
       {
          InitializeComponent();
-         ToolTip toolTip = new ToolTip
+         ToolTip toolTip = new ToolTip // Create ToolTip and associate with the Form container
          {
-            InitialDelay = 1000,
-            ShowAlways = true
+            InitialDelay = 500, // Milliseconds transpired before appearing
+            ShowAlways = true // Force ToolTip text to display
          };
+         // Set all necessary ToolTips
          toolTip.SetToolTip(btnIncrease, "Increase fan speed");
          toolTip.SetToolTip(btnDecrease, "Decrease fan speed");
          toolTip.SetToolTip(gbFanSpeed, "Current fan speed\nClick on the value to display another unit");
          toolTip.SetToolTip(lbFanSpeed, "Current fan speed\nClick here to display another unit");
+         toolTip.SetToolTip(cmbConnected, "Select an available fan");
+         toolTip.SetToolTip(btnConnect, "Click to connect or disconnect the fan");
       }
 
-      private void lbFanSpeed_MouseClick(object sender, MouseEventArgs e)
+      private void lbFanSpeed_MouseClick(object sender, MouseEventArgs e) // Select unit to display
       {
-         if (unit)
+         if (unit) // Set to percentage of total
          {
             gbFanSpeed.Text = "%";
             lbFanSpeed.Text = ((Convert.ToInt32(received) * 100) / 180).ToString();
          }
-         else
+         else // Set to degrees
          {
-            gbFanSpeed.Text = "PWM";
+            gbFanSpeed.Text = "°";
             lbFanSpeed.Text = received;
          }
          unit = !unit;
@@ -52,6 +55,7 @@ namespace AiriosApplication
 
       private void btnIncrease_Click(object sender, EventArgs e)
       {
+         // Increase degree if connected
          if (connected)
          {
             try
@@ -60,13 +64,14 @@ namespace AiriosApplication
             }
             catch
             {
-               SerialDisconnect();
+               SerialDisconnect(); // Shut down application if fan disconnected
             }
          }
       }
 
       private void btnDecrease_Click(object sender, EventArgs e)
       {
+         // Increase degree if connected
          if (connected)
          {
             try
@@ -75,22 +80,22 @@ namespace AiriosApplication
             }
             catch
             {
-               SerialDisconnect();
+               SerialDisconnect(); // Shut down application if fan disconnected
             }
          }
       }
 
       private void btnConnect_Click(object sender, EventArgs e)
       {
-         if (!connected) { SerialConnect(); }
-         else { SerialDisconnect(); }
+         if (!connected) { SerialConnect(); } // Connect if unconnected
+         else { SerialDisconnect(); } // Disconnect if connected
       }
 
       private void SerialDisconnect()
       {
-         if (connected)
+         if (connected) 
          {
-            try
+            try // Try disconnecting normally
             {
                connected = false;
                cmbConnected.Enabled = true;
@@ -101,7 +106,7 @@ namespace AiriosApplication
                btnConnect.Text = "Connect";
                lbFanSpeed.Text = "0";
             }
-            catch
+            catch // Otherwise shut down application
             {
                MessageBox.Show("Device disconnected abruptly. Aborting application.", "Device Disconnected Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                Application.Exit();
@@ -112,7 +117,7 @@ namespace AiriosApplication
       private void SerialConnect()
       {
          string[] serialPorts = SerialPort.GetPortNames();
-         if (serialPorts.Contains(cmbConnected.Text))
+         if (serialPorts.Contains(cmbConnected.Text)) // Add selected COM port
          {
             port = new SerialPort(cmbConnected.Text, 9600, Parity.None, 8, StopBits.One);
             connected = true;
@@ -132,12 +137,13 @@ namespace AiriosApplication
 
       private void VentilationForm_FormClosing(object sender, FormClosingEventArgs e)
       {
-         SerialDisconnect();
+         SerialDisconnect(); // Send stop message to fan
       }
 
       private void SerialReceive()
       {
-         while (connected)
+         // Set string to received data
+         while (connected) 
          {
             try
             {
@@ -159,6 +165,7 @@ namespace AiriosApplication
 
       private void cmbConnected_DropDown(object sender, EventArgs e)
       {
+         // Set dropdown items
          cmbConnected.Items.Clear();
          string[] ports = SerialPort.GetPortNames();
          foreach (string port in ports)
@@ -169,6 +176,7 @@ namespace AiriosApplication
 
       private void cmbConnected_TextChanged(object sender, EventArgs e)
       {
+         // Verify user input by matching available COM port
          string[] serialPorts = SerialPort.GetPortNames();
          if (serialPorts.Contains(cmbConnected.Text))
          {

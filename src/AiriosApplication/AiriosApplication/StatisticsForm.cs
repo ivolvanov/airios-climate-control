700550c8b0a55c1ba3aaf4424740dc326b796aa3
Ivanov, Ivaylo I.L <i432022@student.fontys.nl>
2020-06-16 02:49:37 +0200

    Fixed all known bugs, no crashes will occur

--- src/AiriosApplication/AiriosApplication/StatisticsForm.cs
+++ src/AiriosApplication/AiriosApplication/StatisticsForm.cs
@@ -16,7 +16,6 @@ namespace AiriosApplication
         public StatisticsForm()
         {
             InitializeComponent();
-            this.BackColor = Color.FromArgb(48, 51, 107);
         }
 
         private void StatisticsForm_Load(object sender, EventArgs e)
@@ -32,10 +31,7 @@ namespace AiriosApplication
             toolTip.SetToolTip(chartStats, "Data from sensor visualized");
             toolTip.SetToolTip(gbData, "Collected data from sensors");
 
-            // the default view of the dataTable has the data that is needed for graphing filtered (as in it's in the selected by the user timeframe)
-
-            chartStats.DataSource = Readings.Data.DefaultView;
-            dgvData.DataSource = Readings.Data;
+            dgvData.DataSource = Readings.Data.DefaultView;
             dgvData.Font = cmbDevice.Font;
             cmbDevice.DataSource = ApplicationForm.ConnectedDevices;
 
@@ -45,61 +41,65 @@ namespace AiriosApplication
                 dgvData.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 dgvData.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 dgvData.Columns[i].DefaultCellStyle.BackColor = Color.FromArgb(48, 51, 107);
-                dgvData.Columns[i].DefaultCellStyle.ForeColor = Color.FromArgb(223, 249, 251);                
+                dgvData.Columns[i].DefaultCellStyle.ForeColor = Color.FromArgb(223, 249, 251);
             }
             updateTimer.Start();
         }
 
         private void updateTimer_Tick(object sender, EventArgs e)
-        {
+        {            
             updateTimer.Stop();
-            if (rbTemperature.Checked)
-                Graph("Temperature");
-            else if (rbHumidity.Checked)
-                Graph("Humidity");
-            else if (rbVOC.Checked)
-                Graph("VOC");
-            else if (rbCO2.Checked)
-                Graph("CO2");
+            lock (Readings.readingsLock)
+            {
+                if (rbTemperature.Checked)
+                    Graph("Temperature");
+                else if (rbHumidity.Checked)
+                    Graph("Humidity");
+                else if (rbVOC.Checked)
+                    Graph("VOC");
+                else if (rbCO2.Checked)
+                    Graph("CO2");
+            }
             updateTimer.Start();
         }
 
         private void Graph(string column)
-        {    
+        {
             if (!cbAllTime.Checked)
             {
-                // this beauty here makes sure that if a certain day is selected we only graph the values from that day from the selected device
+                // this beauty here makes sure that if a certain day is selected we only graph the values from that day from the selected device                
                 Readings.Data.DefaultView.RowFilter = "Timestamp > '" + dtpDate.Value.Date + "' and Timestamp < '" + dtpDate.Value.Date.AddDays(1)
-                    + "' AND ID = '" + cmbDevice.SelectedItem.ToString() + "'";
+                  + "' AND ID = '" + cmbDevice.SelectedItem.ToString() + "'";
                 chartStats.Series[0].XValueType = ChartValueType.Time;
             }
             else
             {
-                // if the user wants everything graphed we remove the filter and only leave the device filter
+                // if the user wants everything graphed we remove the filter and only leave the device filter                
                 Readings.Data.DefaultView.RowFilter = "ID = '" + cmbDevice.SelectedItem.ToString() + "'";
                 chartStats.Series[0].XValueType = ChartValueType.DateTime;
-            }                  
+            }
 
-            try
+            // any other way of binding the data I tried causes a threading issue because of the .DataSource property
+            chartStats.DataSource = Readings.Data.DefaultView;
+            chartStats.Series[0].XValueMember = "Timestamp";
+            chartStats.Series[0].YValueMembers = column;
+            chartStats.DataBind();
+
+            // we make a copy of the default view for dynamic scaling - otherwise if one day has high 
+            // values all of them will have a maximal Y axis value that is way too high and the graph looks ugly
+            DataTable dynamicScalingTable;
+                dynamicScalingTable = Readings.Data.DefaultView.ToTable();
+            if (dynamicScalingTable.Rows.Count != 0)    // if a day with no readings is selected
             {
-                chartStats.Series[0].XValueMember = "Timestamp";
-                chartStats.Series[0].YValueMembers = column;
-
-                // we make a copy of the default view for dynamic scaling - otherwise if one day has high 
-                // values all of them will have a maximal Y axis value that is way too high and the graph looks ugly
-                DataTable dynamicScalingTable = Readings.Data.DefaultView.ToTable();
                 chartStats.ChartAreas[0].AxisY.Minimum = Convert.ToDouble(dynamicScalingTable.Compute("MIN(" + column + ")", null)) - 5;
                 chartStats.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(dynamicScalingTable.Compute("MAX(" + column + ")", null)) + 5;
-
-                // temperature varies less so less scaling for it
-                if (column == "Temperature")
-                {
-                    chartStats.ChartAreas[0].AxisY.Minimum += 4;
-                    chartStats.ChartAreas[0].AxisY.Maximum -= 4;
-                }
             }
-            catch (Exception) { }
-            // The compute function causes some problems
+            // temperature varies less so less scaling for it
+            if (column == "Temperature")
+            {
+                chartStats.ChartAreas[0].AxisY.Minimum += 4;
+                chartStats.ChartAreas[0].AxisY.Maximum -= 4;
+            }
         }
 
         private void cbAllTime_CheckedChanged(object sender, EventArgs e)
@@ -108,3 +108,4 @@ namespace AiriosApplication
         }
     }
 }
+
